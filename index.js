f92d46c50d3ce95df2eeca059d1cd8ce327ee0b0
Jiaao Hou <jiaao.hou@yale.edu>
2015-01-12 23:07:20 -0500

    buggy username tracking

--- index.js
+++ index.js
@@ -1,25 +1,47 @@
 var app = require('express')(),
-		http = require('http').Server(app); // server
-		io = require('socket.io')(http);
+		http = require('http').Server(app), // server
+		io = require('socket.io')(http),
+		bodyParser = require('body-parser');
+
+var usernames = {};
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}))
 
 app.get('/', function(req, res) {
 	res.render('index');
 });
+
+app.route('/login')
+.get(function(req, res) {
+	res.render('login');
+})
+.post(function(req, res) {
+	var name = req.body.name;
+	// usernames[] = name;
+	res.redirect('/');
+})
 
 io.on('connection', function(socket) {
 	console.log('a user connected');
 	io.emit('user connected');
 
 	socket.on('chat message', function(msg) {
-		console.log('message: ' + msg);
-		io.emit('chat message', msg);
+		console.log(socket.username + ': ' + msg)
+		io.emit('chat message', socket.username, msg);
+	})
+
+	socket.on('add username', function(username) {
+		socket.username = username;
+		console.log("new username: " + socket.username);
 	})
 
 	socket.on('disconnect', function() {
-		console.log('user disconnected');
-		io.emit('user disconnected');
+		console.log(socket.username + ' disconnected');
+		io.emit('user disconnected', socket.username);
 	})
 })
 
